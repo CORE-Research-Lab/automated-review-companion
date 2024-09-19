50dfbcdc6c5724e756179fa5918f2a5d7466d3c3
Harry Ye <harry.ye@mail.utoronto.ca>
2024-09-19 10:26:00 -0400

    remove pre-populated testing search

--- publication-scraper-web/src/utils/templates.ts
+++ publication-scraper-web/src/utils/templates.ts
@@ -3,7 +3,7 @@ import { SearchEngineType } from "@/types";
 export const defaultSearchForm = {
   validation_papers: [],
   search_terms: {
-    advanced: 'AI and "Machine Learning" and not Education',
+    advanced: '',
     primary: [],
     secondary: [],
     tertiary: [],
