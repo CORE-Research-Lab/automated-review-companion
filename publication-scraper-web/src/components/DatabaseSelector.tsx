997fb5b499d9084ae0bed057f9dd3de386bf637a
Harry Ye <harry.ye@mail.utoronto.ca>
2024-11-29 20:28:36 -0500

    add tooltip in database selection for improved transparency.

--- publication-scraper-web/src/components/DatabaseSelector.tsx
+++ publication-scraper-web/src/components/DatabaseSelector.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { SearchEngineType, SearchForm } from "@/types";
-import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group";
+import {Tooltip} from "@mui/material";
+import {tooltipText} from "@/data/tooltip.tsx";
 
 export interface DatabaseSelectorProps {
   searchForm: SearchForm;
@@ -28,24 +29,32 @@ const DatabaseSelector: React.FC<DatabaseSelectorProps> = (props) => {
   }
   
   return ( 
-      <ToggleGroup type="multiple" className="flex flex-wrap justify-content-start">
-      {
-        Object.values(SearchEngineType).map((source) => (
-            <ToggleGroupItem 
-              key={source} 
-              value={source} 
-              className={cn(searchForm.sources.find((s) => s === source) === source ? "bg-primary" : " bg-white ")}
-              onClick={() => setSearchForm({
-                ...searchForm,
-                sources: searchForm.sources.find((s) => s === source)
-                    ? searchForm.sources.filter((s) => s !== source)
-                    : [...searchForm.sources, source]
-              })}>{parseSearchEngineName(source)}
-            </ToggleGroupItem>
-        ))
-      }
-    </ToggleGroup>
-   );
-}
+       <div className="flex flex-wrap justify-content-start">
+        {Object.values(SearchEngineType).map((source) => (
+          <Tooltip key={source} title={tooltipText.search.databaseDescription[source]}>
+            <div className={cn("input-group-prepend")}>
+              <div
+                className={cn(
+                  "input-group-text h-100 rounded-0",
+                  searchForm.sources.includes(source) ? "bg-primary text-white" : "bg-white"
+                )}
+                onClick={() =>
+                  setSearchForm({
+                    ...searchForm,
+                    sources: searchForm.sources.includes(source)
+                      ? searchForm.sources.filter((s) => s !== source)
+                      : [...searchForm.sources, source],
+                  })
+                }
+                style={{ cursor: "pointer" }}
+              >
+                <span>{parseSearchEngineName(source)}</span>
+              </div>
+            </div>
+          </Tooltip>
+        ))}
+      </div>
+    );
+  };
  
 export default DatabaseSelector;
